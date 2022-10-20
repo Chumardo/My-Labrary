992b4a11389d705ccb268710210a9981c988b431
chumardo <dachigiorgadze98@gmail.com>
2022-10-20 16:10:20 +0400

    show books in table function

--- script.js
+++ script.js
@@ -12,8 +12,22 @@ class Book {
 function addBookToLibrary(title, author, pages, status) {
     const book = new Book(title, author, pages, status);
     myLibrary.push(book);
+    showBooksInLibrary();
 }
 
 addBookToLibrary('title', 'author', 'pages', 'status') 
 addBookToLibrary('The E-Myth', 'Michael E. Gerber', '226', 'read')
 console.log(myLibrary)
+
+function showBooksInLibrary() {
+    const bookList = document.querySelector('#table-body');
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        const bookRow = document.createElement('tr');
+        bookList.appendChild(bookRow);
+
+        // TITLE
+        const title = document.createElement('td');
+        title.textContent = myLibrary[i].title;
+        bookRow.appendChild(title);
+    }
+}
